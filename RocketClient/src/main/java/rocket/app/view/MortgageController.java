c076fba7ece2c7f49962f91932edbfd920641425
Raymond Hennen <rhennen@udel.edu>
2016-05-23 22:54:00 -0400

    Client

--- RocketClient/src/main/java/rocket/app/view/MortgageController.java
+++ RocketClient/src/main/java/rocket/app/view/MortgageController.java
@@ -1,23 +1,16 @@
 package rocket.app.view;
 
-
-import java.text.NumberFormat;
-
-import org.springframework.format.number.CurrencyFormatter;
-
-
 import eNums.eAction;
 import exceptions.RateException;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import rocket.app.MainApp;
 import rocketBase.RateBLL;
-import rocketBase.RateDAL;
 import rocketCode.Action;
 import rocketData.LoanRequest;
 
@@ -25,127 +18,75 @@ public class MortgageController {
 
 	private MainApp mainApp;
 	
-	private static NumberFormat cf = NumberFormat.getCurrencyInstance();
-	private static NumberFormat pf = NumberFormat.getPercentInstance();
-
 	@FXML
-	private TextField txtIncome;
-	
+	private TextField income;
 	@FXML
-	private TextField txtExpenses;
-
+	private TextField expenses;
 	@FXML
-	private TextField txtCreditScore;
-	
+	private TextField creditScore;
 	@FXML
-	private TextField txtHouseCost;
-	
+	private TextField houseCost;
 	@FXML
-	private TextField txtDownPayment;
-
-	ObservableList<String> termList = FXCollections.observableArrayList("15 Years", "30 Years") ;
-	
+	private ComboBox<String> term;
 	@FXML
-	private ComboBox cmbTerm;
-
+	private Label mortgageDetails;
 	@FXML
-	private TextField txtMortgagePayment;
-
+	private Button calculatePayment;
 	@FXML
-	private TextField txtRate;
-	
-	@FXML
-	private Button btnCalcPayment;
-	
-	@FXML
-	private Button btnExit;
-	
-	@FXML
-	private Button btnClear;
-	
-	@FXML
-	private void initialize() {
-		cmbTerm.setItems(termList);
-		cmbTerm.setValue(termList.get(1));
-		
-	}
-
-	
+	private Label interestRateLabel;
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
 	}
 	
 	
+	//	TODO - RocketClient.RocketMainController
+	//			Call this when btnPayment is pressed, calculate the payment
 	@FXML
-	public void btnCalculatePayment(ActionEvent event)
+	public void btnCalculatePayment(ActionEvent event) throws RateException
 	{
-		Object message = null;
 		
 		Action a = new Action(eAction.CalculatePayment);
 		LoanRequest lq = new LoanRequest();
-
-		lq.setdAmount(Double.parseDouble(txtHouseCost.getText()) - Double.parseDouble(txtDownPayment.getText()));
-		lq.setIncome(Double.parseDouble(txtIncome.getText()));
-		lq.setExpenses(Double.parseDouble(txtExpenses.getText()));
-		lq.setiCreditScore(Integer.parseInt(txtCreditScore.getText()));
-		try {
-			lq.setdRate(RateBLL.getRate(Integer.parseInt(txtCreditScore.getText())));
-		} catch (RateException e) {
-			lq.setdRate(-1.0);
-		}
-		
-		if(cmbTerm.getValue() == "15 Years"){
-			lq.setiTerm(15);
-		}
-		else{
-			lq.setiTerm(30);
-		}
+		lq.setiCreditScore(Integer.parseInt(creditScore.getText()));
+		lq.setiTerm(Integer.parseInt(term.getValue()));
+		lq.setdAmount(Double.parseDouble(houseCost.getText()));
+		lq.setdRate(RateBLL.getRate(lq.getiCreditScore()));
+		lq.setdPayment(RateBLL.getPayment(lq.getdRate(), lq.getiTerm()*12, lq.getdAmount(), 0, false));
 		a.setLoanRequest(lq);
-	
-		mainApp.messageSend(lq);
+		Object message = lq;
+		mainApp.messageSend(message);
+		String rate = "";
+		rate += lq.getdRate();
+		
+		interestRateLabel.setText(rate);
+		mortgageDetails.setText("Value to be displayed");
+		
 	}
-	
 	
 	public void HandleLoanRequestDetails(LoanRequest lRequest)
 	{
-		
-		RateBLL RateBLL = new RateBLL();
-		
-		if (lRequest.getdRate() == -1.0) {
-			txtRate.setText("Sorry your credit score does not qualify.");
-			txtMortgagePayment.setText("Sorry your credit score does not qualify.");	
-		
-		} else if(rocketBase.RateBLL.IncomeCheck(lRequest) == false){
-			
-			txtMortgagePayment.setText("House cost too high.");
-			txtRate.setText("House cost too high.");
-			
-		} else {
-			txtMortgagePayment.setText(cf.format(lRequest.getdPayment()));
-			pf.setMaximumFractionDigits(3);
-			txtRate.setText(pf.format(lRequest.getdRate()/100));
+		//	TODO - RocketClient.HandleLoanRequestDetails
+		//			lRequest is an instance of LoanRequest.
+		//			after it's returned back from the server, the payment (dPayment)
+		//			should be calculated.
+		//			Display dPayment on the form, rounded to two decimal places
+		double payment = lRequest.getdPayment();
+		double Income = Double.parseDouble(income.getText());
+		double Expenses = Double.parseDouble(expenses.getText());
+		double max = Income*28/100;
+		if ((Income*36/100)-Expenses < max){
+			max = (Income*36/100)-Expenses;
 		}
-		
-		
-	}
-	
-	@FXML
-	public void btnExit(ActionEvent event)
-	{
-		System.exit(0);
-		
-	}
-	
-	@FXML
-	public void btnClear(ActionEvent event)
-	{
-		txtIncome.setText("");
-		txtExpenses.setText("");
-		txtCreditScore.setText("");
-		txtHouseCost.setText("");
-		txtDownPayment.setText("");
-		txtMortgagePayment.setText("");
-		txtRate.setText("");
+		String message = "";
+		if (max > payment){
+			message += "You do not have enough moeny to afford this house, \n";
+			message += "try finding a cheaper one.";
+		}else{
+			message += "Your monthly payment is: $";
+			message += payment;
+			message += ".";
+		}
+		mortgageDetails.setText(message);
 		
 	}
 }
